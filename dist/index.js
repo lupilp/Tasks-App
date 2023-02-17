3abb4c678671ab13f500735fffc7c418bd2b2943
lupilp <lupilp97@gmail.com>
2023-02-17 18:25:27 -0300

    Revert "commit"
    This reverts commit 539a787364c70efd1141bbcc0219f94236a5e9be.

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-var _app = _interopRequireDefault(require("./app"));
-require("./database");
-_app["default"].listen(3000);
-console.log("Server on port", 3000);
