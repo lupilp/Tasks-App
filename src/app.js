989e47e7b49a1ea12ec202887c41f5b243e5d285
lupilp <lupilp97@gmail.com>
2023-02-17 18:04:49 -0300

    css

--- src/app.js
+++ src/app.js
@@ -20,6 +20,7 @@ app.set("view engine", ".hbs");
 
 app.use(morgan("dev")); // Middleware
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(indexRoutes);
 
